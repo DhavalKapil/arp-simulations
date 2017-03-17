739ced16bab732956b9c04f488a99793290806af
Dhaval Kapil <me@dhavalkapil.com>
2017-03-17 16:43:16 +0530

    Added Receivable interface

--- src/com/secarp/device/Node.java
+++ src/com/secarp/device/Node.java
@@ -1,8 +1,11 @@
 package com.secarp.device;
 
+import java.util.*;
+
 import com.secarp.address.Ipv4Address;
 import com.secarp.address.MacAddress;
 import com.secarp.protocol.Packet;
+import com.secarp.protocol.Receivable;
 
 /**
  * An abstraction of a particular node
@@ -17,12 +20,16 @@ public class Node {
     // Mac address
     private MacAddress macAddress;
 
+    // The registered receivable instances that handle incoming packets
+    private ArrayList<Receivable> receivables;
+
     /**
      * Constructor function
      */
     public Node(Ipv4Address ipv4Address, MacAddress macAddress) {
         this.ipv4Address = ipv4Address;
         this.macAddress = macAddress;
+        this.receivables = new ArrayList<Receivable>();
     }
 
     /**
@@ -52,12 +59,39 @@ public class Node {
         this.macAddress = macAddress;
     }
 
+    public ArrayList<Receivable> getReceivables() {
+        return this.receivables;
+    }
+
+    public void setReceivables(ArrayList<Receivable> receivables) {
+        this.receivables = receivables;
+    }
+
+    /**
+     * Registers a new receivable
+     *
+     * @param Receivable The new receivable to be registered
+     */
+    public void registerReceivable(Receivable receivable) {
+        this.receivables.add(receivable);
+    }
+
     /**
      * Handles a packet receive operation
+     * Iteratively calls all the receivables in a separate thread
      *
      * @param Packet The received packet
      */
     public void handlePacket(Packet packet) {
-        // TODO
+        for (Receivable receivable : this.receivables) {
+            // Spawning a new thread
+            // The good old Java way :)
+            new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        receivable.handlePacket(packet);
+                    }
+                }).start();
+        }
     }
 }
---
+++ src/com/secarp/protocol/Receivable.java
@@ -0,0 +1,14 @@
+package com.secarp.protocol;
+
+/**
+ * An asbtraction for protocols that wait upon incoming packets
+ */
+public interface Receivable {
+    /**
+     * Incoming packet handler
+     * This function is called whenever a packet arrives
+     *
+     * @param packet The received packet
+     */
+    public void handlePacket(Packet packet);
+}
